948076d773be3494c66890b627c7dc3f3202c0ff
Daniel Roy <daniel@thevariousartists.com.au>
2023-11-23 22:04:30 +1000

    changes to only force scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
