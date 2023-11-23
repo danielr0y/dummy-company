5b1cd20e56676154c425ecf65124cc7bae0b8409
Daniel Roy <daniel@thevariousartists.com.au>
2023-11-23 22:08:38 +1000

    removes the test route

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,12 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/test', function () {
-    return Inertia::render('LeaveManager/Test', [
-        "test" => 'what is this'
-    ]);
-})->middleware(['auth', 'verified'])->name('test');
-
-Route::resource('leave', LeaveRequestController::class)->middleware(['auth', 'verified']);
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('leave', LeaveRequestController::class)->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
